27e9c224617cc8cbb67725cbe8ab00f6b26eae05
dkl19 <dkl19@skbl-422-009.ph.ic.ac.uk>
2022-03-04 12:11:38 +0000

    sprite position left to right fixed

--- sprites.s
+++ sprites.s
@@ -1,9 +1,14 @@
 #include <xc.inc>
     
 extrn	LcdOpen, LcdSendData, LcdSelectLeft, LcdSelectRight, LcdSetPage, LcdSetRow, LcdDisplayOn, LcdDisplayOff, LcdReset, LcdClear
-global	make_sprite_x
+global	make_sprite_x, set_y
     
-psect	sprite_data
+psect	udata_acs
+    aaa:          ds     1
+    bbb:	  ds	 1
+    ccc:	  ds	 1
+    x_coord2:	  ds	 1
+    size_right:	  ds	 1
     size:         ds     1
     page_length:  ds	 1
     x_coord:	  ds	 1
@@ -11,26 +16,60 @@ psect	sprite_data
     
 psect	sprite_code, class=CODE
 
+set_y:
+    movwf	y_coord, A
+    return
+    
 make_sprite_x:
     movwf	x_coord, A
-    movlw	0x08
-    movwf	size, A
     movlw	0x40
     movwf	page_length, A
-    subfwb	size, W, A
-    cpfsgt	x_coord, A
+    movlw	0x08
+    movwf	size, A
+    addwf	x_coord, W, A; x2 in W
+    movwf	x_coord2, A
+    decf	x_coord2, F, A
+    movf	x_coord2, W, A
+    cpfsgt	page_length, A; if 64>x2, skip  (all on left)
+    bra		right_size
     bra		displayleft
-    bra	        displayright
+   
     
+right_size:
+    subfwb	page_length, W, A; x2-64
+    movwf	size_right, A; 
+    incf	size_right, F, A
+    movf	page_length, W, A
+    movlw	0x63
+    cpfsgt	x_coord, A; if x1>64 skip
+    bra		inbetween
+    bra		displayright
+
+inbetween:
+    movf	x_coord, W, A
+    subwf	page_length, W, A; 64-x1
+    movwf	size, A
+    call	displayleft
+    movff	size_right, size
+    call        LcdSelectRight
+    movlw	0x00
+    call        LcdSetRow
+    bra		set_page
+    
+
 displayleft:
     call        LcdSelectLeft
     movf	x_coord, W, A
     call        LcdSetRow
  
+set_page:	
+    movf	y_coord, W, A
+    call	LcdSetPage
 display:
     movlw	0xff
     call	LcdSendData
     decfsz      size,F,A
+    ;movf	size, W, A
     bra	        display
     return
     
@@ -39,7 +78,7 @@ displayright:
     movlw	0x40
     subwf	x_coord, W, A
     call        LcdSetRow
-    bra		display
+    bra		set_page
     
     end
     

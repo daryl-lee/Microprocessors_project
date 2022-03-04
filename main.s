20316e74392c2123d1394a63730f542b87561771
dkl19 <dkl19@skbl-422-009.ph.ic.ac.uk>
2022-03-04 12:10:31 +0000

    fixed left to right column transition

--- main.s
+++ main.s
@@ -1,6 +1,6 @@
 #include <xc.inc>
 
-extrn	LcdOpen, LcdSendData, LcdSelectLeft, LcdSelectRight, LcdSetPage, LcdSetRow, LcdDisplayOn, LcdDisplayOff, LcdReset, LcdClear
+extrn	LcdOpen, LcdSendData, LcdSelectLeft, LcdSelectRight, LcdSetPage, LcdSetRow, LcdDisplayOn, LcdDisplayOff, LcdReset, LcdClear, make_sprite_x, set_y
 	
 psect	udata_acs   ; reserve data space in access ram
 
@@ -8,7 +8,6 @@ psect	udata_acs   ; reserve data space in access ram
 psect	udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
 myArray:    ds 0x80 ; reserve 128 bytes for message data
 
-psect	data    
 
     
 psect	code, abs	
@@ -26,34 +25,25 @@ setup:	bcf	CFGS	; point to Flash program memory
 start: 	call	LcdOpen
 	call	LcdDisplayOn
 	
-reset1:	;call    LcdSelectLeft
-	;movlw	0x01
-	;call	LcdSetPage
-;	movlw	0x7F
-;	call	LcdSendData
-;	movlw	0x08
-;	call	LcdSendData
-;	movlw	0x08
-;	call	LcdSendData
-;	movlw	0x08
-;	call	LcdSendData
-;	movlw	0x7F
-;	call	LcdSendData
-;	movlw	0x01
-;	call	LcdSetPage
-;	movlw	0x00
-;	call	LcdSendData
-	
-	;call	LcdReset
-	;bra	reset1
+reset1:	
 	call    LcdClear
 	call    LcdSelectLeft
 	movlw	0x02
+	call	set_y
+	movlw	0x3D
+	call    make_sprite_x
+;	movlw	0x03
+;	call	set_y
+;	movlw	0x38
+;	call    make_sprite_x
+	call    LcdSelectLeft
+	movlw	0x03
 	call	LcdSetPage
-	movlw	0x01
+	movlw	0x3f
 	call    LcdSetRow
-	movlw	0xaa
+	movlw	0xff
 	call	LcdSendData
+	
 	goto	$		; goto current line in code
 
 	end	rst

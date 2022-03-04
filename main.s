89ecf415cea07c248e22599b22de5aaade139ee1
dkl19 <dkl19@skbl-422-009.ph.ic.ac.uk>
2022-03-04 13:08:31 +0000

    loop moving sprite

--- main.s
+++ main.s
@@ -2,8 +2,12 @@
 
 extrn	LcdOpen, LcdSendData, LcdSelectLeft, LcdSelectRight, LcdSetPage, LcdSetRow, LcdDisplayOn, LcdDisplayOff, LcdReset, LcdClear, make_sprite_x, set_y
 	
-psect	udata_acs   ; reserve data space in access ram
 
+psect	udata_acs   ; named variables in access ram
+cnt_l:	ds 1   ; reserve 1 byte for variable LCD_cnt_l
+cnt_h:	ds 1   ; reserve 1 byte for variable LCD_cnt_h
+cnt_ms:	ds 1   ; reserve 1 byte for ms counter
+start_x: ds 1
     
 psect	udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
 myArray:    ds 0x80 ; reserve 128 bytes for message data
@@ -27,23 +31,54 @@ start: 	call	LcdOpen
 	
 reset1:	
 	call    LcdClear
+	movlw	0x00
+	movwf	start_x, A
+	
+loop:	
 	call    LcdSelectLeft
 	movlw	0x02
 	call	set_y
-	movlw	0x3D
+	movf	start_x, W, A
+	;movlw	0x4a
 	call    make_sprite_x
+	movlw   0xaf
+	call	delay_ms
+	call    LcdClear
+	incf	start_x, F, A
+	bra	loop
 ;	movlw	0x03
 ;	call	set_y
 ;	movlw	0x38
 ;	call    make_sprite_x
-	call    LcdSelectLeft
-	movlw	0x03
-	call	LcdSetPage
-	movlw	0x3f
-	call    LcdSetRow
-	movlw	0xff
-	call	LcdSendData
+
 	
 	goto	$		; goto current line in code
+	
+delay_ms:		    ; delay given in ms in W
+	movwf	cnt_ms, A
+delay_sub2:	movlw	250	    ; 1 ms delay
+	call	delay_x4us	
+	decfsz	cnt_ms, A
+	bra	delay_sub2
+	return
+
+delay_x4us:		    ; delay given in chunks of 4 microsecond in W
+	movwf	cnt_l, A	; now need to multiply by 16
+	swapf   cnt_l, F, A	; swap nibbles
+	movlw	0x0f	    
+	andwf	cnt_l, W, A ; move low nibble to W
+	movwf	cnt_h, A	; then to LCD_cnt_h
+	movlw	0xf0	    
+	andwf	cnt_l, F, A ; keep high nibble in LCD_cnt_l
+	call	delay
+	return
+
+delay:			; delay routine	4 instruction loop == 250ns	    
+	movlw 	0x00		; W=0
+delay_sub1:	
+	decf 	cnt_l, F, A	; no carry when 0x00 -> 0xff
+	subwfb 	cnt_h, F, A	; no carry when 0x00 -> 0xff
+	bc 	delay_sub1		; carry, then loop again
+	return	
 
 	end	rst

dc9ef1fcaaf0bfa6852735a9578ab60e8cd8d665
dkl19 <dkl19@skbl-422-009.ph.ic.ac.uk>
2022-03-15 10:12:07 +0000

    collision logic working (both trees)

--- collision.s
+++ collision.s
@@ -1,7 +1,7 @@
 #include <xc.inc>
 
 extrn	t1_x1, t2_x1, d_y1
-global	collision
+global	collision_t1, collision_t2
     
 psect	udata_acs
 d_x1: ds 1
@@ -10,20 +10,20 @@ delta_y: ds 1
 	
 psect	collision_code, class=CODE
 	
-collision:
+collision_t1:
     movlw   0x2a
     movwf   d_x1, A
     cpfsgt  t1_x1, A ;skip if t1_x1>d_x1
-    bra	    collision_neg    	;find d_x1-t1_x1
+    bra	    collision_neg_t1   	;find d_x1-t1_x1
     subwf   t1_x1, W, A	;t1_x1-d_x1
     movwf   delta_x, A
     movlw   0x07
     cpfsgt  delta_x, A ;skip if delta_x>7 (>=8)
-    bra	    collision_y
+    bra	    collision_y_t1
     movlw   0x00    ;W set to 0 if no collision 
     return  ;no collision
     
-collision_y:
+collision_y_t1:
     movlw   0x08
     subwf   d_y1, W, A
     movwf   delta_y, A
@@ -33,13 +33,46 @@ collision_y:
     movlw   0x00
     return  ;no collision
     
-collision_neg:
+collision_neg_t1:
     movf    t1_x1, W, A
     subwf   d_x1, W, A
     movwf   delta_x, A
     movlw   0x07
     cpfsgt  delta_x, A ;skip if delta_x>7 (>=8)
-    bra	    collision_y
+    bra	    collision_y_t1
+    movlw   0x00
+    return  ;no collision
+    
+collision_t2:
+    movlw   0x2a
+    movwf   d_x1, A
+    cpfsgt  t2_x1, A ;skip if t1_x1>d_x1
+    bra	    collision_neg_t2   	;find d_x1-t1_x1
+    subwf   t2_x1, W, A	;t1_x1-d_x1
+    movwf   delta_x, A
+    movlw   0x07
+    cpfsgt  delta_x, A ;skip if delta_x>7 (>=8)
+    bra	    collision_y_t2
+    movlw   0x00    ;W set to 0 if no collision 
+    return  ;no collision
+    
+collision_y_t2:
+    movlw   0x08
+    subwf   d_y1, W, A
+    movwf   delta_y, A
+    movlw   0x0f
+    cpfsgt  delta_y, A ;skip if delta_y>15 (>=16)
+    bra     collision_set_bool	;collision occurs, W set to 1
+    movlw   0x00
+    return  ;no collision
+    
+collision_neg_t2:
+    movf    t2_x1, W, A
+    subwf   d_x1, W, A
+    movwf   delta_x, A
+    movlw   0x07
+    cpfsgt  delta_x, A ;skip if delta_x>7 (>=8)
+    bra	    collision_y_t2
     movlw   0x00
     return  ;no collision
     

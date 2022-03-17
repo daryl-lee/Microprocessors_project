7d0f41cfb244e3f6ad152646852689f1210cb2c3
dkl19 <dkl19@skbl-422-009.ph.ic.ac.uk>
2022-03-17 11:56:49 +0000

    rng

---
+++ random.s
@@ -0,0 +1,31 @@
+#include <xc.inc>
+
+extrn	    seed
+global	    random_setup, update_seed
+    
+psect	udata_acs
+five:	    ds  1
+seventeen:  ds	1
+    
+    
+psect	rand_code, class=CODE
+
+random_setup:
+	movlw	0x05
+	movwf	five, A
+	movlw	0x11
+	movwf	seventeen, A
+	
+	return
+
+update_seed:	
+	movf	seed, W ;
+	mulwf	five, A ; ARG1 * ARG2 ->
+	movf	PRODL, W, A
+	addwf	seed, F, A
+	movlw	0b0111111
+	andwf	seed, F, A
+	
+	return
+
+

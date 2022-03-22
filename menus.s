176fa5240c67876626bbafaa5ff20e927edf44c6
dkl19 <dkl19@skbl-422-009.ph.ic.ac.uk>
2022-03-22 11:06:09 +0000

    start and stop menu

--- menus.s
+++ menus.s
@@ -1,7 +1,7 @@
 #include <xc.inc>
 extrn		set_y, make_sprite_x
 extrn		load_data_A, load_data_E, load_data_G , load_data_M, load_data_O, load_data_R, load_data_V, load_data_P, load_data_S
-global		start_menu    
+global		start_menu, end_menu    
     
 psect	menu_code, class=CODE    
 start_menu:
@@ -31,6 +31,51 @@ start_menu:
     movlw	0x5a
     call	make_sprite_x
     
+    return
+    
+end_menu:
+    movlw	0x02
+    call	set_y
+    call	load_data_G
+    movlw	0x32
+    call	make_sprite_x
+
+
+    call	load_data_A
+    movlw	0x3a
+    call	make_sprite_x
+
+
+    call	load_data_M
+    movlw	0x42
+    call	make_sprite_x
+
+
+    call	load_data_E
+    movlw	0x4a
+    call	make_sprite_x
+
+    movlw	0x03
+    call	set_y
+    call	load_data_O
+    movlw	0x32
+    call	make_sprite_x
+
+
+    call	load_data_V
+    movlw	0x3a
+    call	make_sprite_x
+
+
+    call	load_data_E
+    movlw	0x42
+    call	make_sprite_x
+
+
+    call	load_data_R
+    movlw	0x4a
+    call	make_sprite_x
+
     return
 
 

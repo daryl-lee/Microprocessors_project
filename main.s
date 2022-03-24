22079ef9dc8ee4ce028ea15a25407228733a6d48
dkl19 <dkl19@skbl-422-009.ph.ic.ac.uk>
2022-03-24 11:56:11 +0000

    comments

--- main.s
+++ main.s
@@ -2,7 +2,6 @@
 
 extrn	LcdOpen, LcdSendData, LcdSelectLeft, LcdSelectRight, LcdSetPage, LcdSetRow, LcdDisplayOn, LcdDisplayOff, LcdReset, LcdClear, make_sprite_x, set_y
 extrn	set_x, make_sprite_y, LcdSetStart, key_setup, key_setup_column, key_delay_ms, key_setup_row, decode, collision_t1, collision_t2
-extrn	load_data_A, load_data_E, load_data_G , load_data_M, load_data_O, load_data_R, load_data_V, load_data_P, load_data_S
 extrn	load_data_treetop, load_data_treebottom, load_data_dino
 extrn	random_setup, update_seed
 extrn	score_init, scoreboard, highscore, display_score, display_hscore,  hscore_low, hscore_high
@@ -71,11 +70,11 @@ init:
 	movlw	0x18
 	movwf	distance, A ;min distance between trees
 	movlw	0x7f
-	movwf	t1_x1, A
+	movwf	t1_x1, A    ;starting x1 coordinate of tree 1
 	movwf	display_distance, A ;127
 	movlw	0x8f
 	addwf	distance, W, A
-	movwf	t2_x1, A
+	movwf	t2_x1, A    ;starting x1 coordinate of tree 2
 	movlw	0x08
 	movwf	start_y, A
 	movlw   0x50
@@ -90,36 +89,41 @@ init:
 startup:
 	call	start_menu
 	
+	movlw	0x50
+	call	delay_ms
+	
 	movlw	0x00
-	movwf	start_counter, A
+	movwf	start_counter, A    ;initialise counter for seed generation
 
 set_seed:
 	movff	start_counter, seed
-	incf	start_counter, F, A
+	incf	start_counter, F, A	; increment counter until FFh then goes back to 00h
 	call	key_press
 	movwf	key_bool, A
 	movlw   0x01
 	cpfslt	key_bool, A; don't skip if key is pressed
-	bra	random_init
+	bra	random_init ;set counter to seed if key is pressed
 	bra	set_seed
 	
 random_init:
-	call	update_seed
+	call	update_seed ;generates the next random number in the sequence 
 	movlw	0b00111111
-	movwf	short_seed, A
-	movlw	0x10
-	movwf	min_dist, A
+	movwf	short_seed, A	;ensure that new tree position does not exceed FFh
+	movlw	0x16
+	movwf	min_dist, A	;minimum distance between t1_x1 and t2_x1
 	movlw	0x7f
-	addwf	seed, W, A
-	movwf	t1_x1, A
+	addwf	seed, W, A	
+	movwf	t1_x1, A	;starting tree 1 position = 127 + random number (0-127)
 	call    update_seed
 	movf	seed, W, A
 	andwf	short_seed, W, A
 	addwf	t1_x1, W, A
 	addwf	min_dist, W, A
-	movwf	t2_x1, A
+	movwf	t2_x1, A	;starting tree 2 position = tree 1 position + minimum distance + short random number(0 - 64)
 	call    LcdClear
-loop:	
+	
+	; ******* Ground State ****************************************	
+loop:				    
     
 	call    LcdSelectLeft
 	call	make_trees
@@ -165,6 +169,8 @@ loop:
 	
 	goto	loop
 	
+	
+	; ******* Airborne State ****************************************	
 jump:
 	;call	pulse
 	call    LcdSelectLeft
@@ -445,29 +451,29 @@ jump:
 	
 	
 reset1:
-	movff	t2_x1, t1_x1
+	movff	t2_x1, t1_x1		;swap tree labels
 	movf	t1_x1, W, A
 	cpfslt	display_distance, A	; dont skip if t1_x1 is less than 127
 	bra	random_case1
-	call    update_seed
+	call    update_seed		; if there are no trees on screen
 	movf	seed, W, A
 	andwf	short_seed, W, A
 	addwf	t1_x1, W, A
 	addwf	min_dist, W, A
-	movwf	t2_x1, A
-	movf	delay_rate, W, A
-	subwf	delay_time, F, A
+	movwf	t2_x1, A		;tree 2 position = tree 1 position + minimum distance + short random number
+	movf	delay_rate, W, A	
+	subwf	delay_time, F, A	;decrease delay time by delay rate every time we make a tree
 
 	
 	return	
 	
-random_case1:
+random_case1:				;if there is another tree on screen already
 	call    update_seed
 	movf	seed, W, A
 	andwf	short_seed, W, A
 	addwf	display_distance, W, A
 	addwf	min_dist, W, A
-	movwf	t2_x1, A
+	movwf	t2_x1, A		; tree 2 position = 127 + minimum distance + short random number
 	return
 	
 key_press:	
@@ -513,26 +519,26 @@ delay_sub1:
 	return	
 	
 make_trees:
-	movlw	0x06
+	movlw	0x06			;page of upper tree 1 sprite
 	call	set_y
 	call	load_data_treebottom
 	movf	t1_x1, W, A
 	cpfslt	display_distance, A	; display tree if x is less than 127
 	call    make_sprite_x
-	movlw	0x05
+	movlw	0x05			;page of lower tree 1 sprite
 	call	set_y
 	call	load_data_treetop
 	movf	t1_x1, W, A
 	cpfslt	display_distance, A
 	call    make_sprite_x
 	
-	movlw	0x06
+	movlw	0x06			;page of upper tree 2 sprite
 	call	set_y
 	call	load_data_treebottom
 	movf	t2_x1, W, A
 	cpfslt	display_distance, A
 	call    make_sprite_x
-	movlw	0x05
+	movlw	0x05			;page of lower tree 2 sprite
 	call	set_y
 	call	load_data_treetop
 	movf	t2_x1, W, A
@@ -542,7 +548,7 @@ make_trees:
 	return
 	
 	
-move_trees:
+move_trees:			;both trees move by 4 pixels per frame
 	decf	t1_x1, F, A
 	decf	t1_x1, F, A
 	decf	t1_x1, F, A
@@ -556,14 +562,14 @@ move_trees:
 
 	
 collision_check:
-	call	collision_t1
+	call	collision_t1	   ; check with collision box of tree 1
 	
 	movwf	collision_bool, A
 	movlw   0x01
 	cpfslt	collision_bool, A ;skip if no collision (W=0x00)
 	bra	game_over
 	
-	call	collision_t2
+	call	collision_t2	    ;check with collision box of tree 2
 	
 	movwf	collision_bool, A
 	movlw   0x01
@@ -581,9 +587,9 @@ game_over:
 display_game_over:
 	call	end_menu
 	
-	call	display_score
-	call	highscore
-	call	display_hscore
+	call	display_score	    ;display current score
+	call	highscore	    ;updates highscore if current score > previous high score
+	call	display_hscore	    ;display highscore
 	
 	movlw	0x90
 	call	delay_ms

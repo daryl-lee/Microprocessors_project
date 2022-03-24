34857b8922b9ef42aa0f65c3b24e6ee418b2ef56
dkl19 <dkl19@skbl-422-009.ph.ic.ac.uk>
2022-03-24 16:28:37 +0000

    updated menus (with pause menu)

--- menus.s
+++ menus.s
@@ -1,33 +1,107 @@
 #include <xc.inc>
 extrn		set_y, make_sprite_x
-extrn		load_data_A, load_data_E, load_data_G , load_data_M, load_data_O, load_data_R, load_data_V, load_data_P, load_data_S
-global		start_menu, end_menu    
+extrn		load_data_A, load_data_E, load_data_G , load_data_M, load_data_O, load_data_R, load_data_V, load_data_P, load_data_S, load_data_U, load_data_D
+extrn		load_data_B, load_data_L, load_data_Y
+extrn		load_data_pause, load_data_colon, load_data_dino
+global		start_menu, end_menu, pause_menu    
     
 psect	menu_code, class=CODE    
 start_menu:
+    movlw	0x02
+    call	set_y
+    call	load_data_O
+    movlw	0x22
+    call	make_sprite_x
+    
+    call	load_data_colon
+    movlw	0x32
+    call	make_sprite_x
+    
+
+    call	load_data_P
+    movlw	0x42
+    call	make_sprite_x
+    
+    call	load_data_L
+    movlw	0x4a
+    call	make_sprite_x
+    
+    call	load_data_A
+    movlw	0x52
+    call	make_sprite_x
+    
+    call	load_data_Y
+    movlw	0x5a
+    call	make_sprite_x
+    
+    movlw	0x04
+    call	set_y
+    call	load_data_B
+    movlw	0x22
+    call	make_sprite_x
+    
+    call	load_data_colon
+    movlw	0x32
+    call	make_sprite_x
+    
+
+    call	load_data_P
+    movlw	0x42
+    call	make_sprite_x
+    
+    call	load_data_A
+    movlw	0x4a
+    call	make_sprite_x
+    
+    call	load_data_U
+    movlw	0x52
+    call	make_sprite_x
+    
+    call	load_data_S
+    movlw	0x5a
+    call	make_sprite_x
+    
+    call	load_data_E
+    movlw	0x62
+    call	make_sprite_x
+ 
+    movlw	0x07
+    call	set_y
+    call	load_data_dino
+    movlw	0x05
+    call	make_sprite_x
+    
+    return
+    
+pause_menu:
     movlw	0x03
     call	set_y
     call	load_data_P
-    movlw	0x1a
-    call	make_sprite_x
-
-    call	load_data_R
     movlw	0x22
     call	make_sprite_x
-
-    call	load_data_E
+    
+    call	load_data_A
     movlw	0x2a
     call	make_sprite_x
-
-    call	load_data_S
+    
+    call	load_data_U
     movlw	0x32
     call	make_sprite_x
-
+    
     call	load_data_S
     movlw	0x3a
     call	make_sprite_x
-
-    call	load_data_O
+    
+    call	load_data_E
+    movlw	0x42
+    call	make_sprite_x
+    
+    call	load_data_D
+    movlw	0x4a
+    call	make_sprite_x
+    
+    
+    call	load_data_pause
     movlw	0x5a
     call	make_sprite_x
     
